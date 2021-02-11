34cef4e4fe4fdc2f4b9d6c49fcb1c994c58f0224
MacbookAir2019 <macbookair2019@MacBook-Air-Admin.local>
2021-02-11 20:39:56 +0600

    popytka 7

--- src/helpers/constant.jsx
+++ src/helpers/constant.jsx
@@ -1,3 +1,2 @@
+export const JSON_API = 'https://team-project-aizh.herokuapp.com/api';
 export const AUTH_API = 'http://35.223.58.241:5000';
-export const JSON_API = 'https://team-project-aizh.herokuapp.com/api'; 
-
